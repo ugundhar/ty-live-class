f79b07eda85977f7419cdfcddd5910eef9866362
I741825 <ugundhar>
2026-02-12 18:20:12 +0530

    llm setup completed

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from live-yt-classes!")
+# Import statements
+import os
+from dotenv import load_dotenv
+from gen_ai_hub.proxy.native.openai import OpenAI
 
+# Load environment variables
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+# Initialize the SAP AI Core OpenAI client
+client = OpenAI()
+
+query="what is the llm"
+# Call GPT-4o
+response = client.chat.completions.create(
+    model_name="gpt-4o",
+    messages=[
+        {"role": "user", "content": query}
+    ]
+)
+
+# Print the response
+print(response.choices[0].message.content)
